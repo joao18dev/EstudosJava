b1a5b160ab6366437c44a2122752d18e578181d8
João Guilherme <joaoguilhermelima0306@gmail.com>
2025-12-17 12:40:01 -0300

    Adicioanando novo arquivo, outro curso relacionado a Java.

---
+++ CursoFernandaKipper/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        var minhaIdade = 19;
+        var meuNome = "João";
+
+        System.out.println("Olá, meu nome é " + meuNome + " e eu tenho " + minhaIdade + " anos.");
+        
+    }
+}
