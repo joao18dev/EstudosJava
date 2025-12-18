56f09f5839edd519ce196185ba598ba5b2ab96d9
João Guilherme <joaoguilhermelima0306@gmail.com>
2025-12-18 10:25:20 -0300

    Estudo Java: Aprendizado sobre tipos primitivos

---
+++ CursoFernandaKipper/tiposprimitivos.java
@@ -0,0 +1,15 @@
+package CursoFernandaKipper;
+
+public class tiposprimitivos {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float  f = 10.5f;
+        double d = 20.5;
+        char c = 'a';
+        boolean bool = true;
+        String str = " João ";
+    }
+}
