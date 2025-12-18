2182f2ef4002169d2aab5f5c5173f76197aa531b
João Guilherme <joaoguilhermelima0306@gmail.com>
2025-12-18 10:35:09 -0300

    Estudo Java: Condicionais simples 'If e Else' junto aos tipos primitivos.

---
+++ CursoFernandaKipper/condicionais.java
@@ -0,0 +1,22 @@
+package CursoFernandaKipper;
+
+public class condicionais {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float  f = 10.5f;
+        double d = 20.5;
+        char c = 'a';
+        boolean bool = true;
+        String str = " João ";
+
+        if (b > 99) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+}
