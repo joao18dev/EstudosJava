ad19cbd124621e6d701e70ce28c037d2660088ae
João Guilherme <joaoguilhermelima0306@gmail.com>
2025-12-18 11:19:58 -0300

    Estudo Java: Aprendizagem sobre ' Vetor ' com a criação de ' heights '.

---
+++ CursoFernandaKipper/vetor.java
@@ -0,0 +1,22 @@
+package CursoFernandaKipper;
+
+public class vetor {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float  f = 10.5f;
+        double d = 20.5;
+        char c = 'a';
+        boolean bool = true;
+        String str = " João ";
+
+        int [] colecaoDeInteiros = {1, 2, 3, 4, 5};
+        String [] colecaoDeStrings = {"João", "Gabriel", "Pedro", "Gustavo"};
+
+        System.out.println(colecaoDeInteiros[0]);
+        System.out.println(colecaoDeStrings[0]);
+
+    }
+} 
