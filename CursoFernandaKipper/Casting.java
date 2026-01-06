4d1d432d6b7d9dd206f8b1cf9f4d184f464e5a6d
João Guilherme <joaoguilhermelima0306@gmail.com>
2026-01-06 14:57:56 -0300

    Estudo Java: Manipulação de dados = Conversão de uma dado para outro.

---
+++ CursoFernandaKipper/Casting.java
@@ -0,0 +1,14 @@
+package CursoFernandaKipper;
+
+import java.util.ArrayList;
+
+public class Casting {
+    public static void main(String[] args) {
+
+        String meuString = "10";
+        int meuInt = Integer.parseInt(meuString);
+
+        String minhaString = String.valueOf(meuInt);
+
+    }
+}
