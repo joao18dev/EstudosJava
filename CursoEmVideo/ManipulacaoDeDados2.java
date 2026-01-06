bdcdcc17538f1370942bd27a8c9f96691397676c
João Guilherme <joaoguilhermelima0306@gmail.com>
2026-01-06 11:30:45 -0300

    Estudo Java: Manipulação de dados = Saídas de dados com String + Float.

---
+++ CursoEmVideo/ManipulacaoDeDados2.java
@@ -0,0 +1,9 @@
+public class ManipulacaoDeDados2 {
+
+    public static void main(String[] args){
+        String nome = "João";
+        float nota = 9.5f;
+
+        System.out.println("A nota de " + nome + " é " + nota);
+    }
+}
